e8be6c1a27421aafe1a5a25e8e42a118c2f7515a
Jose Pinto <pintojose.benedicto2@gmail.com>
2019-12-12 05:32:31 -0700

    Dashboard and inquiries features

--- btre/settings.py
+++ btre/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.humanize'
 ]
 
@@ -140,3 +141,9 @@ MEDIA_URL = '/media/'
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'password'
+EMAIL_USE_TLS = True
